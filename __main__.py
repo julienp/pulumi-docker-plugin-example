1a2beec77f4e15e4fc789337f7c3bb748e136776
Julien Poissonnier <julien@caffeine.lu>
2025-03-31 14:47:46 +0100

    v0.0.3

--- __main__.py
+++ __main__.py
@@ -1,6 +1,6 @@
 from pulumi.provider.experimental import component_provider_host
 
-from .component import Greeter
+from component import Greeter
 
 if __name__ == "__main__":
     component_provider_host(name="greeter", components=[Greeter])
